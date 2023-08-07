b1c38a5e3de4ecd49ce384b194129b622582e369
Bina Hovav <binahovav@gmail.com>
2023-08-07 17:08:05 +0300

    Add images to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,21 +24,21 @@
         <p class="top-box-content">Simple, flexible, and powerful. All it takes are boards, lists, and cards to get a clear view of who’s doing what and what needs to get done.</p>
       </div>
       <div class="left-box">
-        <button>
+        <button @click="changeImage('boards')">
           <h3>Boards</h3>
           <p>Mello boards keep tasks organized and work moving forward. In a glance, see everything from “things to do” to “aww yeah, we did it!</p>
         </button>
-        <button>
+        <button @click="changeImage('lists')">
           <h3>Lists</h3>
           <p>The different stages of a task. Start as simple as To Do, Doing or Done—or build a workflow custom fit to your team’s needs. There’s no wrong way to Mello.</p>
         </button>
-        <button>
+        <button @click="changeImage('cards')">
           <h3>Cards</h3>
           <p>Cards represent tasks and ideas and hold all the information to get the job done. As you make progress, move cards across lists to show their status.</p>
         </button>
       </div>
       <div class="right-box">
-        <img src="https://images.ctfassets.net/rz1oowkt5gyp/3N2U3C71rApm61cGFxnc2E/970b010002488a09a420282df5e7b43a/Carousel_Image_Boards_2x.png?w=1536&fm=webp" />
+        <img :src="currentImage" :class="{ 'slide-in': showImage }" />
       </div>
     </div>
   </main>
@@ -50,11 +50,31 @@ import HomepageHeader from '../cmps/HomepageHeader.vue'
 export default {
   name: 'home',
   data() {
-    return {}
+    return {
+      currentImage: 'https://images.ctfassets.net/rz1oowkt5gyp/3N2U3C71rApm61cGFxnc2E/970b010002488a09a420282df5e7b43a/Carousel_Image_Boards_2x.png?w=1536&fm=webp',
+      showImage: false,
+    }
   },
   computed: {},
   created() {},
-  methods: {},
+  methods: {
+    changeImage(option) {
+      switch (option) {
+        case 'boards':
+          this.currentImage = 'https://images.ctfassets.net/rz1oowkt5gyp/3N2U3C71rApm61cGFxnc2E/970b010002488a09a420282df5e7b43a/Carousel_Image_Boards_2x.png?w=1536&fm=webp'
+          break
+        case 'lists':
+          this.currentImage = 'https://images.ctfassets.net/rz1oowkt5gyp/4U0VUZYX2tQmB5KVGxBabp/7321ac088fe8ec39dbe3069c47d7df99/Carousel_Image_Lists_2x.png?w=1536&fm=webp'
+          break
+        case 'cards':
+          this.currentImage = 'https://images.ctfassets.net/rz1oowkt5gyp/26CA6JZeRgoOK4nuRHnIlY/060702a80cf7c3be3651d9297d196267/Carousel_Image_Cards_2x.png?w=1536&fm=webp'
+          break
+        default:
+          this.currentImage = 'https://images.ctfassets.net/rz1oowkt5gyp/3N2U3C71rApm61cGFxnc2E/970b010002488a09a420282df5e7b43a/Carousel_Image_Boards_2x.png?w=1536&fm=webp'
+      }
+      this.showImage = true
+    },
+  },
   components: { HomepageHeader },
 }
 </script>
